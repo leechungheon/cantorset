7b8fd530b734e4926b2145fff724349fd4f12cc7
dawn_lch <chung4111@naver.com>
2024-02-16 15:10:26 +0900

    초기 커밋.

---
+++ 소스.c
@@ -0,0 +1,25 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <math.h>
+void f(int n);
+int main() {
+    int n;
+    while (scanf("%d", &n) != EOF) {
+        f(pow(3, n));
+        printf("\n");
+    }
+    return 0;
+}
+void f(int n) {
+    if (n != 1) {
+        n /= 3;
+        f(n);
+        for (int i = 0; i < n; i++) {
+            printf(" ");
+        }
+        f(n);
+    }
+    else {
+        printf("-");
+    }
+}
